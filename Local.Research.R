0e4d5b8d546ab162d548ababaf3bb747a4789e18
VictoriaDiCesare <79263847+VictoriaDiCesare@users.noreply.github.com>
2023-07-17 17:07:18 +0200

    compute country rankings

--- Local.Research.R
+++ Local.Research.R
@@ -15,6 +15,13 @@ pubs.totals <- read.csv2("~/Desktop/Local.Research/journals_total.csv")
 pubs.totals <- subset(pubs.totals, pubs.totals$source_type == "Journal", select = c("source_id", "pubs"))
 colnames(pubs.totals) <- c("journal.id","pubs.n")
 
+# read file with refs totals per journal
+refs.totals <- read.csv2("~/Desktop/Local.Research/journals_total_refs.csv")
+refs.totals <- subset(refs.totals, refs.totals$source_type == "Journal", select = c("source_id", "refs"))
+colnames(refs.totals) <- c("journal.id","refs.n")
+
+# read file with cits totals per journal
+
 # subset journals source type
 df.journals <- subset(df, df$source.type == "Journal", select = c("pubs.refs.cits", "journal.id", "journal.name", "issn", "eissn", "publisher", "country", "count"))
 
@@ -25,6 +32,7 @@ df.journals$pubs.refs.cits <- as.factor(df.journals$pubs.refs.cits)
 df.journals.aggr <- df.journals %>% group_by(journal.id, journal.name, country, pubs.refs.cits) %>% 
                       summarise(count = sum(count), .groups = 'drop') %>%
                       as.data.frame()
+
 
 ##
 # reshape to wide format in order to work individually with the variables pubs, refs and cits
@@ -62,6 +70,7 @@ df.journals.wide <- df.journals.wide %>%
                       mutate(refs.country.n = n_distinct(country[!is.na(refs)])) %>%
                     ungroup()
 
+
 ##
 # filter cits per journal to obtain the countries with max cits values
 df.journals.max.cits <- df.journals.wide %>%
@@ -92,7 +101,8 @@ df.journals.max.refs <- select(df.journals.max.refs, -c(pubs, cits, pubs.n, cits
 
 # merge all dataframes with cits, pubs and refs max values
 df.journals.max <- merge(merge(df.journals.max.cits, df.journals.max.pubs, by = c("journal.id", "journal.name"), all = TRUE), df.journals.max.refs, by = c("journal.id", "journal.name"), all = TRUE)
-write.csv2(df.journals.max, file = "~/Desktop/Local.Research/dataset_journals_max.csv")
+#write.csv2(df.journals.max, file = "~/Desktop/Local.Research/dataset_journals_max.csv")
+
 
 ##
 # Figure 1. Plot histogram of Gini Coefficient per individual journals per cits, pubs and refs
@@ -107,7 +117,7 @@ ggplot(df.figure1) +
   facet_wrap(~cits.pubs.refs) +
   xlab("Gini Coefficient") +
   ylab("Journal count")
-ggsave("~/Desktop/Local.Research/Figure1.jpg")
+#ggsave("~/Desktop/Local.Research/Figure1.jpg")
 
 # Figure 2. Plot scatterplot of Gini Coefficient per number of countries in each journal per cits, pubs and refs
 df.figure2 <- subset(df.journals.wide, select = c("journal.id", "cits.country.n", "pubs.country.n", "refs.country.n"))
@@ -121,7 +131,7 @@ ggplot(df.figure2) +
   theme_minimal() +
   xlab("Gini Coefficient") +
   ylab("Country count per journal")
-ggsave("~/Desktop/Local.Research/Figure2.jpg")
+#ggsave("~/Desktop/Local.Research/Figure2.jpg")
 
 # Figure 2A. Plot scatterplot of Gini Coefficient per number of countries in each journal per cits
 df.figure2A <- filter(df.figure2, cits.pubs.refs == "cits")
@@ -130,7 +140,7 @@ ggplot(df.figure2A) +
   theme_minimal() +
   xlab("Gini Coefficient") +
   ylab("Country count per journal")
-ggsave("~/Desktop/Local.Research/Figure2A.jpg")
+#ggsave("~/Desktop/Local.Research/Figure2A.jpg")
 
 # Figure 2B. Plot scatterplot of Gini Coefficient per number of countries in each journal per pubs
 df.figure2B <- filter(df.figure2, cits.pubs.refs == "pubs")
@@ -139,7 +149,7 @@ ggplot(df.figure2B) +
   theme_minimal() +
   xlab("Gini Coefficient") +
   ylab("Country count per journal")
-ggsave("~/Desktop/Local.Research/Figure2B.jpg")
+#ggsave("~/Desktop/Local.Research/Figure2B.jpg")
 
 # Figure 2C. Plot scatterplot of Gini Coefficient per number of countries in each journal per refs
 df.figure2C <- filter(df.figure2, cits.pubs.refs == "refs")
@@ -148,51 +158,139 @@ ggplot(df.figure2C) +
   theme_minimal() +
   xlab("Gini Coefficient") +
   ylab("Country count per journal")
-ggsave("~/Desktop/Local.Research/Figure2C.jpg")
+#ggsave("~/Desktop/Local.Research/Figure2C.jpg")
 
+##
 # Table 1A. Filter journals with only one country per cits
 df.table1A <- filter(df.figure2, cits.pubs.refs == "cits" & country.n == 1)
 df.table1A <- merge(x = df.table1A, y = df.journals.wide[,c("journal.id", "journal.name")], by = "journal.id")
 df.table1A <- df.table1A %>% distinct()
-write.csv2(df.table1A, file = "~/Desktop/Local.Research/Table1A.csv")
+df.table1A <- merge(df.table1A, df.journals.max.cits[,c("journal.id", "cits.max.country")], by = "journal.id")
+colnames(df.table1A) <- c("journal.id", "cits.pubs.refs", "country.n", "gini.coefficient", "journal.name", "country")
+#write.csv2(df.table1A, file = "~/Desktop/Local.Research/Table1A.csv")
+
+ranking1A <- df.table1A %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+             as.data.frame()
 
 # Table 1B. Filter journals with only one country per pubs
 df.table1B <- filter(df.figure2, cits.pubs.refs == "pubs" & country.n == 1)
 df.table1B <- merge(x = df.table1B, y = df.journals.wide[,c("journal.id", "journal.name")], by = "journal.id")
 df.table1B <- df.table1B %>% distinct()
-write.csv2(df.table1B, file = "~/Desktop/Local.Research/Table1B.csv")
+df.table1B <- merge(df.table1B, df.journals.max.pubs[,c("journal.id", "pubs.max.country")], by = "journal.id")
+colnames(df.table1B) <- c("journal.id", "cits.pubs.refs", "country.n", "gini.coefficient", "journal.name", "country")
+#write.csv2(df.table1B, file = "~/Desktop/Local.Research/Table1B.csv")
+
+ranking1B <- df.table1B %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+             as.data.frame()
 
 # Table 1C. Filter journals with only one country per refs
 df.table1C <- filter(df.figure2, cits.pubs.refs == "refs" & country.n == 1)
 df.table1C <- merge(x = df.table1C, y = df.journals.wide[,c("journal.id", "journal.name")], by = "journal.id")
 df.table1C <- df.table1C %>% distinct()
-write.csv2(df.table1C, file = "~/Desktop/Local.Research/Table1C.csv")
+df.table1C <- merge(df.table1C, df.journals.max.refs[,c("journal.id", "refs.max.country")], by = "journal.id")
+colnames(df.table1C) <- c("journal.id", "cits.pubs.refs", "country.n", "gini.coefficient", "journal.name", "country")
+#write.csv2(df.table1C, file = "~/Desktop/Local.Research/Table1C.csv")
+
+ranking1C <- df.table1C %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+             as.data.frame()
 
 # Table 1. Filter journals with only one country per cits, pubs and refs in the same case
 df.table1 <- merge(merge(df.table1A[,c("journal.id", "journal.name", "country.n")], df.table1B[,c("journal.id", "journal.name", "country.n")], by = c("journal.id", "journal.name")), df.table1C[,c("journal.id", "journal.name", "country.n")], by = c("journal.id", "journal.name"))
 colnames(df.table1) <- c("journal.id", "journal.name", "cits.country.n","pubs.country.n", "refs.country.n")
 df.table1 <- merge(df.table1, df.journals.max[,c("journal.id", "cits.max.country", "pubs.max.country", "refs.max.country")], by = c("journal.id"))
-write.csv2(df.table1, file = "~/Desktop/Local.Research/Table1.csv")
+#write.csv2(df.table1, file = "~/Desktop/Local.Research/Table1.csv")
+
+ranking1 <- df.table1 %>%
+              group_by(journal.id) %>%
+              summarize(country = unique(c(cits.max.country, pubs.max.country, refs.max.country))) %>%
+            ungroup()
+
+ranking1 <- ranking1 %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+            as.data.frame()
+
+# Table 0. Filter journals with the same country per cits, pubs and refs per journal
+df.table0 <- filter(df.table1, cits.max.country == pubs.max.country & pubs.max.country == refs.max.country)
+colnames(df.table0) <- c("journal.id", "journal.name", "cits.country.n", "pubs.country.n", "refs.country.n", "country", "country.pubs", "country.refs")
+
+ranking0 <- df.table0 %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+            as.data.frame()
+
+# Table 0A. Filter journals with the same country per pubs and cits per journal
+df.table0A <- filter(df.table1, cits.max.country == pubs.max.country)
+colnames(df.table0A) <- c("journal.id", "journal.name", "cits.country.n", "pubs.country.n", "refs.country.n", "country", "country.pubs", "country.refs")
+
+ranking0A <- df.table0A %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+             as.data.frame()
+
+# Table 0B. Filter journals with the same country per pubs and refs per journal
+df.table0B <- filter(df.table1, refs.max.country == pubs.max.country)
+colnames(df.table0B) <- c("journal.id", "journal.name", "cits.country.n", "pubs.country.n", "refs.country.n", "country.cits", "country", "country.refs")
+
+ranking0B <- df.table0B %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+             as.data.frame()
 
 # Table 2. Find same max country between cits and pubs per journal
 df.table2 <- merge(x = df.journals.max.cits[,c("journal.id", "journal.name", "cits.max.country")], y = df.journals.max.pubs[,c("journal.id", "journal.name", "pubs.max.country")], by = c("journal.id", "journal.name"))
 df.table2 <- filter(df.table2, cits.max.country == pubs.max.country)
-write.csv2(df.table2, file = "~/Desktop/Local.Research/Table2.csv")
+colnames(df.table2) <- c("journal.id", "journal.name", "country", "pubs.max.country")
+#write.csv2(df.table2, file = "~/Desktop/Local.Research/Table2.csv")
+
+ranking2 <- df.table2 %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+            as.data.frame()
 
 # Table 3. Find same max country between refs and pubs per journal
 df.table3 <- merge(x = df.journals.max.refs[,c("journal.id", "journal.name", "refs.max.country")], y = df.journals.max.pubs[,c("journal.id", "journal.name", "pubs.max.country")], by = c("journal.id", "journal.name"))
 df.table3 <- filter(df.table3, refs.max.country == pubs.max.country)
-write.csv2(df.table3, file = "~/Desktop/Local.Research/Table3.csv")
+colnames(df.table3) <- c("journal.id", "journal.name", "country", "pubs.max.country")
+#write.csv2(df.table3, file = "~/Desktop/Local.Research/Table3.csv")
+
+ranking3 <- df.table3 %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+            as.data.frame()
 
 # Table 4. Find same max country between cits, pubs and refs per journal
 df.table4 <- merge(merge(df.journals.max.cits[,c("journal.id", "journal.name", "cits.max.country")], df.journals.max.pubs[,c("journal.id", "journal.name", "pubs.max.country")], by = c("journal.id", "journal.name")), df.journals.max.refs[,c("journal.id", "journal.name", "refs.max.country")], by = c("journal.id", "journal.name"))
 df.table4 <- filter(df.table4, (cits.max.country == pubs.max.country) & (pubs.max.country == refs.max.country))
-write.csv2(df.table4, file = "~/Desktop/Local.Research/Table4.csv")
+colnames(df.table4) <- c("journal.id", "journal.name", "country", "pubs.max.country", "refs.max.country")
+#write.csv2(df.table4, file = "~/Desktop/Local.Research/Table4.csv")
 
+ranking4 <- df.table4 %>%
+              group_by(country) %>%
+              summarise(journals.n = n(),.groups = 'drop') %>%
+              arrange(desc(journals.n)) %>%
+            as.data.frame()
+
+##
 # Figure 3. Correlogram (dataframe = journal.id, journal.name, pubs.n, refs.n, cits.n, pubs.gini, refs.gini, cits.gini, pubs.country.n, refs.country.n, cits.country.n)
 df.figure3 <- subset(df.journals.wide, select = c("journal.id", "journal.name", "pubs.gini", "refs.gini", "cits.gini", "pubs.country.n", "refs.country.n", "cits.country.n"))
 df.figure3 <- df.figure3 %>% distinct()  
-df.figure3 <- merge(x = df.figure3, y = pubs.totals, by = "journal.id")
+df.figure3 <- merge(merge(df.figure3, pubs.totals, by = "journal.id"), refs.totals, by = "journal.id", all = TRUE)
 
-corr.matrix <- cor(df.figure3[,c("pubs.n", "pubs.gini", "refs.gini", "cits.gini", "pubs.country.n", "refs.country.n", "cits.country.n")])
-#obviar NAs
+corr.matrix <- cor(df.figure3[,c("pubs.n", "refs.n", "pubs.gini", "refs.gini", "cits.gini", "pubs.country.n", "refs.country.n", "cits.country.n")], use = "complete.obs")
+corrplot(corr.matrix, method = "number")
